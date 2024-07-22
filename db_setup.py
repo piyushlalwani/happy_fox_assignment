66e2190d9b1d9976b13f96db3d12a7fee0d029fa
Piyush Lalwani <piyushlalwani143@gmail.com>
2024-07-22 16:05:14 +0530

    Configured db schema - added emails table

---
+++ db_setup.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Base = declarative_base()
+
+class Email(Base):
+    __tablename__ = 'emails'
+    id = Column(Integer, primary_key=True)
+    email_id = Column(String, unique=True, nullable=False)
+    from_email = Column(String, nullable=False)
+
+engine = create_engine('sqlite:///emails.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
